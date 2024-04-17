cb5fc9c2ef65170cbb03b202167178d4ad701819
filipBirve <birve.f@gmail.com>
2024-04-17 22:00:16 +0200

    add argument to generate plots and put plots in demo

--- R/generate_data_lm.R
+++ R/generate_data_lm.R
@@ -73,7 +73,6 @@ checks <- function(x,
 #'   Close to zero mean and high variance, one different mean and sd per target gene (same across all regulator genes). This arranges it so that we have one slope per cell cluster.
 #' REGULATOR MEANS AND SD:
 #'   One mean and sd per cell cluster. It might be easier just to keep it the same for all cell clusters.
-#' TODO: Go through the theory and assumptions of the model: https://en.wikipedia.org/wiki/Multivariate_normal_distribution
 #' @param n_cell_clusters The number of cell clusters.
 #' @param n_target_gene_type The number of target gene types. We have x named target genes that have one expression per cell.
 #' @param n_regulator_gene_type  The number of regulator gene types. We have x named regulator genes that have one expression per cell.
@@ -101,7 +100,8 @@ generate_data_lm <- function(n_cell_clusters = 3,
                              regulator_means = c(1, 2, 3),  # Regulator mean expression in each cell cluster.
                              regulator_standard_deviations = c(0.1, 0.2, 0.3),  # Regulator sd for expression in each cell cluster.
                              coefficients_standard_deviation = 100, # 'The betas/slopes'. One per target gene. Instead of providing mean and standard deviation for each target gene, we provide the standard deviation from which these will be generated. Mean will be 0.
-                             target_gene_type_standard_deviation = 3
+                             target_gene_type_standard_deviation = 3,
+                             plot_stuff = F
 ) {
 
 
@@ -114,6 +114,7 @@ generate_data_lm <- function(n_cell_clusters = 3,
   # regulator_standard_deviations <- c(0.1, 0.2, 0.3)  # For generating dummy data, regulator mean in each cell cluster
   # coefficients_standard_deviation <- 100 # 'The betas/slopes'. One per target gene. Instead of providing mean and standard deviation for each target gene, we provide the standard deviation from which these will be generated. Mean will be 0.
   # target_gene_type_standard_deviation <- 3
+  # Plot_stuff = F
 
 
   # Check arguments -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -169,7 +170,6 @@ generate_data_lm <- function(n_cell_clusters = 3,
   betas <- vector(mode = 'list', length = n_cell_clusters)
   for (i_cell_cluster in 1:n_cell_clusters) {
 
-
     #assuming different targets can have different regulatory models this is more accurate
     temp_betas_for_cell_cluster <- rnorm(n_regulator_gene_type * n_target_gene_type,
                                          mean = 0,
@@ -182,15 +182,27 @@ generate_data_lm <- function(n_cell_clusters = 3,
                          nrow = n_regulator_gene_type,
                          ncol = n_target_gene_type)
 
-
     colnames(temp_betas) <- paste0("t", 1:n_target_gene_type)
     rownames(temp_betas) <- paste0("r", 1:n_regulator_gene_type)
 
     betas[[i_cell_cluster]] <- tibble::as_tibble(temp_betas)
   }
 
-
   # Calculate target gene expressions for each cell cluster ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+  # First calculate them normally and then add noise separately
+    # For easier future plotting
+
+  predicted_cell_cluster_target_gene_expression <- lapply(1:n_cell_clusters,
+                                                          function(i_cell_cluster) {
+                                                            current_betas <- betas[[i_cell_cluster]]
+                                                            # This picks out the relevant betas, as many as there are target gene types in this cell cluster
+                                                            current_regulator_gene_index <- which(true_cell_cluster_allocation == i_cell_cluster)
+                                                            current_regulator_gene_expressions <- regulator_gene_expression[current_regulator_gene_index,]
+                                                            return(as.matrix(current_regulator_gene_expressions) %*% as.matrix(current_betas))
+                                                          }
+  )
+
+  # Todo: remove redundant matrix multiplication
   cell_cluster_target_gene_expression <- lapply(1:n_cell_clusters,
                                                 function(i_cell_cluster) {
                                                   current_betas <- betas[[i_cell_cluster]]
@@ -204,11 +216,17 @@ generate_data_lm <- function(n_cell_clusters = 3,
                                                 }
   )
 
+
+
   # Put it into a tibble
   target_gene_expression <- do.call(rbind, cell_cluster_target_gene_expression)
   colnames(target_gene_expression) <- paste0("t", 1:n_target_gene_type)
   target_gene_expression <- tibble::as_tibble(target_gene_expression)
 
+  #also for predicted values
+  predicted_target_gene_expression <- do.call(rbind, predicted_cell_cluster_target_gene_expression)
+  colnames(predicted_target_gene_expression) <- paste0("t_hat", 1:n_target_gene_type)
+  predicted_target_gene_expression <- tibble::as_tibble(predicted_target_gene_expression)
 
   # Construct output data structure (also a tibble) -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
   dat <- tibble::tibble(cell_id = (1:n_total_cells),
@@ -216,8 +234,143 @@ generate_data_lm <- function(n_cell_clusters = 3,
                         target_gene_expression,
                         regulator_gene_expression)
 
+  if(plot_stuff){
+    # Generate plots for visualisation
+
+    demo_path <- here::here("demo")
+    R_path <- here::here("R")
+    output_path <- demo_path
+
+    vis_dat <- tibble::tibble(cell_id = (1:n_total_cells),
+                              true_cell_cluster_allocation = factor(true_cell_cluster_allocation),
+                              targets     = target_gene_expression,
+                              regulators  = regulator_gene_expression,
+                              predictions = predicted_target_gene_expression,
+                              residuals   = (target_gene_expression - predicted_target_gene_expression)
+    )
+
+    # Visualise the regulator genes, color cells by cluster
+    # This could be done using some kind of principal components, but
+    #   We generate target genes independently of each other so why bother
+
+    plot_pca <- function(indata     = vis_dat$regulators,
+                         cluster_allocations = vis_dat$true_cell_cluster_allocation,
+                         plot_title = "Principal components of regulator genes"
+    ){
+
+      library(ggmulti)
+      library(ggplot2)
+      library(dplyr)
+      library(ggfortify)
+
+      # Apply PCA
+      pca_result <- prcomp(indata, scale. = TRUE)
+
+      # Extract principal components
+      pc_df <- as_tibble(pca_result$x[,1:2])
+
+      # Add labels (if you have any)
+      pc_df$label <- cluster_allocations
+
+      # Plot the first two principal components
+      plot_name <- "regulators_pca.png"
+
+      # png(file.path(demo_path,plot_name))
+      pc_df[sample(nrow(pc_df), 1000),] %>%
+        ggplot( aes(x = PC1, y = PC2, colour = label)) +
+        geom_point(alpha = 0.1) +
+        theme_minimal() +
+        labs(title = plot_title) +
+        scale_color_discrete(name = "Cell cluster")
+      # dev.off()
+    }
+
+    # Visualise the regulator genes
+    png(file.path(output_path, paste0("lm_data_gen_regulators.png")),
+        width = 1024, height = 480, units = "px")
+
+    plot_pca(indata     = vis_dat$regulators,
+             plot_title = "Principal components of regulator genes") -> p
+
+    print(p)
+
+    dev.off()
+
+    # Visualise the target gene expressions
+
+    png(file.path(output_path, paste0("lm_data_gen_targets.png")),
+        width = 1024, height = 480, units = "px")
+
+    plot_pca(indata = vis_dat$targets,
+             plot_title = "Principal components of target genes") -> p
+
+    print(p)
+    dev.off()
+
+    # visualise the betas somehow
+
+    # Convert the list of matrices to a data frame for ggplot
+    df <- do.call(rbind, lapply(seq_along(betas), function(x) {
+      data.frame(matrix = x, as.data.frame(betas[[x]]))
+    }))
+
+    # Convert the data frame from wide to long format
+    df %>% tidyr::pivot_longer(cols = starts_with("t")) -> df_long
+
+    # Create a faceted histogram
+    png(file.path(output_path, paste0("lm_data_gen_betas_hist.png")),
+        width = 1024, height = 480, units = "px")
+
+    ggplot(df_long, aes(x = value)) +
+      geom_histogram(bins = 10
+      ) +
+      theme_minimal() +
+      theme(#panel.grid.major = element_blank()#,
+        panel.grid.minor = element_blank()
+      )  +
+      facet_grid(matrix ~ name, scales = "free", space = "free") +
+      scale_x_continuous(breaks = c(-coefficients_standard_deviation,
+                                    #0,
+                                    coefficients_standard_deviation) ) +
+      scale_y_continuous(breaks = NULL) +
+      xlab("target genes") + ylab("cell clusters") -> p
+
+    print(p)
+
+    dev.off()
+
+
+    # Visualise the mapping
+    # since we generat everything as independent regressions we might as well
+    # just plot one of the regression i guess
+    vis_dat$residuals %>%
+      tidyr::pivot_longer(cols = starts_with("t")) -> residuals_long
+
+    # residuals_long  %>%
+    #   group_by(name) %>%
+    #   summarise(sd=sd(value), mean = mean(value)) -> temp_stat
+
+    png(file.path(output_path, paste0("lm_data_gen_residuals.png")),
+        width = 1024, height = 480, units = "px")
+
+    ggplot(residuals_long, aes(x = value)) +
+      geom_density(
+      ) +
+      theme_minimal() +
+      theme(#panel.grid.major = element_blank()#,
+        panel.grid.minor = element_blank()
+      )  +
+      facet_grid(cols = vars(name), scales = "free", space = "free") +
+      scale_x_continuous(breaks = c(-target_gene_type_standard_deviation,
+                                    #0,
+                                    target_gene_type_standard_deviation) ) -> p
+      print(p)
+    dev.off()
+  }
+
   return(list(dat = dat, true_betas = betas))
-  # return indexes of different gene types?
+
+  # return indexes of different gene ?
   # list(dat = dat,
   #      Z_r = Z_r,
   #      Pi  = Pi,
@@ -241,10 +394,11 @@ if (sys.nframe() == 0) {
                           n_target_gene_type = 5,  # We have x named target genes that have one expression per cell
                           n_regulator_gene_type = 10,  # We have x named regulator genes that have one expression per cell
                           n_cells = c(1000, 2000, 30000),
-                          regulator_means = linspace(1, 10, n = 3),  # Regulator mean expression in each cell cluster.
-                          regulator_standard_deviations = linspace(0.1, 2, n = 3),  # Regulator sd for expression in each cell cluster.
+                          regulator_means = pracma::linspace(1, 10, n = 3),  # Regulator mean expression in each cell cluster.
+                          regulator_standard_deviations = pracma::linspace(0.1, 2, n = 3),  # Regulator sd for expression in each cell cluster.
                           coefficients_standard_deviation = 100, # 'The betas/slopes'. One per target gene. Instead of providing mean and standard deviation for each target gene, we provide the standard deviation from which these will be generated. Mean will be 0.
-                          target_gene_type_standard_deviation = 3
+                          target_gene_type_standard_deviation = 3,
+                          plot_stuff = T
   )
   true_betas <- dat$true_betas
   dat <- dat$dat
@@ -256,3 +410,6 @@ if (sys.nframe() == 0) {
   print(dat)
   print(true_betas)
 }
+
+
+
