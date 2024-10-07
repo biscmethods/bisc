801c76560f061da0732984672694437334cf0595
filipBirve <birve.f@gmail.com>
2024-10-07 17:44:21 +0200

    save results for later use

--- demo/simulated_data_to_stats_pipeline/step4_run_biclustbiclust_many_times.R
+++ demo/simulated_data_to_stats_pipeline/step4_run_biclustbiclust_many_times.R
@@ -1,15 +1,30 @@
 
 
-biclustbiclust_results_list <- vector(mode = "list", length = length(generated_complicated_data_list))
 
+if (!file.exists(file.path(output_path, "biclustbiclust_results_list.rds")) |
+    redo_flag) {
 
-for(iter in 1:length(generated_complicated_data_list )){
+  biclustbiclust_results_list <- vector(mode = "list", length = length(generated_complicated_data_list))
 
-  biclustbiclust_results_list[[iter]] <- bicluctbiclust(
-    data = as.matrix(generated_complicated_data_list[[iter]]$dat[, 1:n_target_genes])
+  for(iter in 1:length(generated_complicated_data_list )){
+
+    biclustbiclust_results_list[[iter]] <- bicluctbiclust(
+      data = as.matrix(generated_complicated_data_list[[iter]]$dat[, 1:n_target_genes])
+    )
+
+  }
+
+  saveRDS(
+    biclustbiclust_results_list,
+    file.path(output_path, "biclustbiclust_results_list.rds")
   )
 
+} else {
+
+  biclustbiclust_results_list    <- readRDS(file.path(output_path, "biclustbiclust_results_list.rds"))
+
 }
+
 
 
 
--- demo/simulated_data_to_stats_pipeline/step5_run_biclust_screg_many_times.R
+++ demo/simulated_data_to_stats_pipeline/step5_run_biclust_screg_many_times.R
@@ -1,13 +1,35 @@
 
-biclust_screg_results_list <- vector(mode = "list", length = length(generated_complicated_data_list))
 
 
-for(iter in 1:length(generated_complicated_data_list )){
 
-  biclust_screg_results_list[[iter]] <- run_biclust_scregclust(
-    dat = generated_complicated_data_list[[iter]]$dat[, 1:n_target_genes]
+
+
+if (!file.exists(file.path(output_path, "biclust_screg_results_list.rds")) |
+    redo_flag) {
+
+
+  biclust_screg_results_list <- vector(mode = "list", length = length(generated_complicated_data_list))
+
+  for(iter in 1:length(generated_complicated_data_list )){
+
+    biclust_screg_results_list[[iter]] <- run_biclust_scregclust(
+      dat = generated_complicated_data_list[[iter]]$dat
+    )
+
+  }
+
+  saveRDS(
+    biclust_screg_results_list,
+    file.path(output_path, "biclust_screg_results_list.rds")
   )
 
+} else {
+
+  biclust_screg_results_list    <- readRDS(file.path(output_path, "biclust_screg_results_list.rds"))
+
 }
+
+
+
 
 
