b47f2b2bce381ddc7d8c3463e3e6e6b9b1096a4c
filipBirve <birve.f@gmail.com>
2024-10-15 22:01:05 +0200

    add titles to latex tables

--- demo/simulated_data_to_stats_pipeline/step7_make_tables.R
+++ demo/simulated_data_to_stats_pipeline/step7_make_tables.R
@@ -1,11 +1,15 @@
-stats2table <- function(tibble, outfile = '/out_temp.tex'){
+stats2table <- function(tibble, outfile = '/out_temp.tex', title = ''){
   summary_stats <- summary(gene_data$value)
   summary_df <- data.frame(Statistic = names(summary_stats), Value = as.numeric(summary_stats))
 
 
   library(xtable)
   latex_table <- xtable(summary_df)
-  latex_code <- print(latex_table, type = "latex", include.rownames = FALSE)
+  latex_code <- print(latex_table,
+                      type = "latex",
+                      include.rownames = FALSE,
+                      caption = title,
+                      caption.placement = "top")
 
 
   sink(paste0(output_path,'/',outfile))
@@ -13,6 +17,6 @@ stats2table <- function(tibble, outfile = '/out_temp.tex'){
   sink()
 }
 
-stats2table(cell_data, 'cell_data.tex')
-stats2table(gene_data, 'gene_data.tex')
-stats2table(biclust_data, 'biclust_data.tex')
+stats2table(cell_data,    'cell_data.tex',    title = 'Rand Indexes on cell cluster allocation')
+stats2table(gene_data,    'gene_data.tex',    title = 'Rand Indexes on gene module allocation')
+stats2table(biclust_data, 'biclust_data.tex', title = 'Rand Indexes on bicluster allocation')
