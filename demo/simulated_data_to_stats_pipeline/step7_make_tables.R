b3438915736051f9dc26353d623c2438194cfd5c
filipBirve <birve.f@gmail.com>
2024-10-16 22:06:39 +0200

    fix captions

--- demo/simulated_data_to_stats_pipeline/step7_make_tables.R
+++ demo/simulated_data_to_stats_pipeline/step7_make_tables.R
@@ -1,22 +1,23 @@
-stats2table <- function(tibble, outfile = '/out_temp.tex', title = ''){
-  summary_stats <- summary(gene_data$value)
+stats2table <- function(tibble, outfile = '/out_temp.tex', caption = 'Your caption here'){
+  summary_stats <- summary(tibble$value)
   summary_df <- data.frame(Statistic = names(summary_stats), Value = as.numeric(summary_stats))
-
 
   library(xtable)
   latex_table <- xtable(summary_df)
   latex_code <- print(latex_table,
                       type = "latex",
                       include.rownames = FALSE,
-                      caption = title,
-                      caption.placement = "top")
+                      booktabs = TRUE)
 
+  # Insert the caption after \begin{table}[ht]
+  latex_code_ <- gsub("\\\\begin\\{table\\}\\[ht\\]", paste0("\\\\begin{table}[ht]\n\\\\caption{", caption, "}"), latex_code)
 
-  sink(paste0(output_path,'/',outfile))
-  cat(latex_code)
+  sink(paste0(output_path, "/", outfile))
+  cat(latex_code_)
   sink()
 }
 
-stats2table(cell_data,    'cell_data.tex',    title = 'Rand Indexes on cell cluster allocation')
-stats2table(gene_data,    'gene_data.tex',    title = 'Rand Indexes on gene module allocation')
-stats2table(biclust_data, 'biclust_data.tex', title = 'Rand Indexes on bicluster allocation')
+# Example usage
+stats2table(tibble = cell_data,    outfile = 'cell_data.tex',    caption = 'Rand Indexes on cell cluster allocation')
+stats2table(tibble = gene_data,    outfile = 'gene_data.tex',    caption = 'Rand Indexes on gene module allocation')
+stats2table(tibble = biclust_data, outfile = 'biclust_data.tex', caption = 'Rand Indexes on bicluster allocation')
