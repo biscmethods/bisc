e990378990e467bd2343047b9742d70bae85a0b1
Sebastian Franzén <seb.franzen@gmail.com>
2024-12-14 19:20:25 +0100

    Filip version of network plot

--- demo/simulated_data_to_stats_runseeds_pipeline/step8_plot_network_temp.R
+++ demo/simulated_data_to_stats_runseeds_pipeline/step8_plot_network_temp.R
@@ -20,67 +20,97 @@ library(concaveman)
 scenarios_to_plot <- c(1,12)
 set.seed(51124)
 
+# Function to create an edge list with color-coded edges
+create_colored_edge_list <- function(true_S_list) {
+  # Combine all edge lists from multiple cell clusters
+  all_edge_lists <- lapply(true_S_list, function(true_S_matrix) {
+    # Get non-zero entries
+    non_zero_entries <- which(true_S_matrix != 0, arr.ind = TRUE)
+
+    # Create edge data frame
+    edge_df <- data.frame(
+      from = paste0("Regulator ", non_zero_entries[, 2]),
+      to = paste0("Target Gene Module ", non_zero_entries[, 1]),
+      weight = true_S_matrix[non_zero_entries],
+      color = ifelse(true_S_matrix[non_zero_entries] > 0, "red", "blue"),
+      stringsAsFactors = FALSE
+    )
+
+    return(edge_df)
+  })
+
+  # Combine edge lists if multiple cell clusters
+  edge_list <- do.call(rbind, all_edge_lists)
+
+  return(edge_list)
+}
+
+
+# Create a function to process the data and create an edge list
+create_edge_list <- function(data) {
+  edge_list <- data.frame()
+  for (cluster in seq_along(data)) {
+    for (module in 1:nrow(data[[cluster]])) {
+      for (gene in 1:ncol(data[[cluster]])) {
+        if (data[[cluster]][module, gene] != 0) {
+          edge_list <- rbind(edge_list, data.frame(
+            from = paste0("Regulator ", gene),
+            # to = paste0("Module_", module, "_Cluster_", cluster)
+            to = paste0("Target Gene Module ", module)
+          ))
+        }
+      }
+    }
+  }
+  return(edge_list)
+}
+
+
+create_group_list <- function(true_S){
+  group_list <- vector(mode="list", length=length(true_S))
+
+  for (cluster in seq_along(true_S)) {
+    true_S[[cluster]][true_S[[cluster]]==-1] <- 1
+    ones <- true_S[[cluster]]==1
+    negative_ones <- true_S[[cluster]]==-1
+    ones <- which_rows(ones)
+    negative_ones <- which_rows(negative_ones)
+    for(i_module in seq_along(ones)){
+
+      if(length(ones[[i_module]])>1){
+        group_list[[cluster]][paste0("Regulator group ", i_module)] <- list(ones[[i_module]])
+      }
+    }
+    for(i_module in seq_along(negative_ones)){
+      if(length(negative_ones[[i_module]])>1){
+        group_list[[cluster]][paste0("Regulator group ", i_module)] <- list(negative_ones[[i_module]])
+      }
+    }
+
+    # for (module in 1:nrow(true_S[[cluster]])) {
+    #   for (gene in 1:ncol(true_S[[cluster]])) {
+    #     if (data[[cluster]][module, gene] != 0) {
+    #       edge_list <- rbind(edge_list, data.frame(
+    #         from = paste0("Gene_", gene),
+    #         to = paste0("Module_", module, "_Cluster_", cluster)
+    #       ))
+    #     }
+    #   }
+    # }
+  }
+  return(group_list)
+}
+
+which_rows <- function(mat) {
+  lapply(seq_len(nrow(mat)), function(i) {
+    row_log <- if (sum(mat[i,]) == 1) rep(FALSE, ncol(mat)) else mat[i,]
+    row_true <- which(mat[i,])
+
+  })
+}
+
+
 for(i_scenario in scenarios_to_plot){
-  # Create a function to process the data and create an edge list
-  create_edge_list <- function(data) {
-    edge_list <- data.frame()
-    for (cluster in seq_along(data)) {
-      for (module in 1:nrow(data[[cluster]])) {
-        for (gene in 1:ncol(data[[cluster]])) {
-          if (data[[cluster]][module, gene] != 0) {
-            edge_list <- rbind(edge_list, data.frame(
-              from = paste0("Regulator ", gene),
-              # to = paste0("Module_", module, "_Cluster_", cluster)
-              to = paste0("Target Gene Module ", module)
-            ))
-          }
-        }
-      }
-    }
-    return(edge_list)
-  }
-
-  which_rows <- function(mat) {
-    lapply(seq_len(nrow(mat)), function(i) {
-      row_log <- if (sum(mat[i,]) == 1) rep(FALSE, ncol(mat)) else mat[i,]
-      row_true <- which(mat[i,])
-
-    })
-  }
-
-  create_group_list <- function(true_S){
-    group_list <- vector(mode="list", length=length(true_S))
-    for (cluster in seq_along(true_S)) {
-      ones <- true_S[[cluster]]==1
-      negative_ones <- true_S[[cluster]]==-1
-      ones <- which_rows(ones)
-      negative_ones <- which_rows(negative_ones)
-      for(i_module in seq_along(ones)){
-
-        if(length(ones[[i_module]])>1){
-          group_list[[cluster]][paste0("Positive regulator module ", i_module)] <- list(ones[[i_module]])
-        }
-      }
-      for(i_module in seq_along(negative_ones)){
-        if(length(negative_ones[[i_module]])>1){
-          group_list[[cluster]][paste0("Negative regulator module ", i_module)] <- list(negative_ones[[i_module]])
-        }
-      }
-
-      # for (module in 1:nrow(true_S[[cluster]])) {
-      #   for (gene in 1:ncol(true_S[[cluster]])) {
-      #     if (data[[cluster]][module, gene] != 0) {
-      #       edge_list <- rbind(edge_list, data.frame(
-      #         from = paste0("Gene_", gene),
-      #         to = paste0("Module_", module, "_Cluster_", cluster)
-      #       ))
-      #     }
-      #   }
-      # }
-    }
-    return(group_list)
-  }
-
   # true_S is a list with n_cell_cluster matrices. Each matrix is n_module rows and n_regulator cols.
   # The matrices have 1, -1 or 0. E.g. 1 in (1,3) if cell module 3 is affected positively by regulator 1.
   # [[1]]
@@ -99,16 +129,26 @@ for(i_scenario in scenarios_to_plot){
   for(i_cell_cluster in seq_along(scenarios[[i_scenario]]$generated_data$true_S)){
     pdf(file.path(output_path, paste0("network_scenario_", i_scenario,"_cell_cluster",i_cell_cluster,".pdf")), width =  7, height = 7)
 
-    edge_list <- create_edge_list(list(scenarios[[i_scenario]]$generated_data$true_S[[i_cell_cluster]]))
-    group_list <- create_group_list(list(scenarios[[i_scenario]]$generated_data$true_S[[i_cell_cluster]]))
+    # Get current true_S matrix
+    true_S <- scenarios[[i_scenario]]$generated_data$true_S[[i_cell_cluster]]
+
+    # Create edge list with colors
+    edge_list <- create_colored_edge_list(list(true_S))
+
+    group_list <- create_group_list(list(true_S))
     group_list <- unlist(group_list, recursive=FALSE)
 
 
     # Create the graph
-    g <- igraph::graph_from_data_frame(edge_list, directed = FALSE)
+    edge_list_pos <- edge_list  # We create a positive only version because the layouting doesn't work with negative weights
+    edge_list_pos$weight <- 1
+    g_pos <- igraph::graph_from_data_frame(edge_list_pos, directed = TRUE)
+    g <- igraph::graph_from_data_frame(edge_list, directed = TRUE)
+
 
     # Set node types (gene or module)
-    V(g)$type <- ifelse(grepl("Gene", V(g)$name), "Gene", "Module")
+    V(g)$type <- ifelse(grepl("Regulator ", V(g)$name), "Regulator ", "Target Gene Module ")
+    V(g_pos)$type <- ifelse(grepl("Regulator ", V(g)$name), "Regulator ", "Target Gene Module")
 
     # Prepare group list for geom_mark_rect
     # Convert group names to node names
@@ -119,23 +159,25 @@ for(i_scenario in scenarios_to_plot){
     # the fill gets an additional alpha value for transparency:
     group_color_fill <- paste0(group_color, '20')
 
-    plot_layout <- igraph::layout_with_fr(g)  # Fruchterman-Reingold layout
+    plot_layout <- igraph::layout_with_fr(g_pos)  # Fruchterman-Reingold layout
 
     if(length(group_ids)>0){
       par(mar = rep(0.1, 4))   # reduce margins
     }
-    plot(g, vertex.color = 'white', vertex.size = 9,
-         edge.color = rgb(0.5, 0.5, 0.5, 0.2),
+    plot(g,
+         vertex.color = ifelse(V(g)$type == "Regulator ", "lightblue", "lightgreen"),
+         vertex.size = 9,
+         edge.color = E(g)$color,
          mark.groups = group_ids,
          mark.col = group_color_fill,
          mark.border = group_color,
          layout = plot_layout)
-    if(length(group_ids)>0){
-      legend('topright', legend = names(group_ids),
-             col = group_color,
-             pch = 15, bty = "n",  pt.cex = 1.5, cex = 0.8,
-             text.col = "black", horiz = FALSE)
-    }
+    # if(length(group_ids)>0){
+    #   legend('topright', legend = names(group_ids),
+    #          col = group_color,
+    #          pch = 15, bty = "n",  pt.cex = 1.5, cex = 0.8,
+    #          text.col = "black", horiz = FALSE)
+    # }
     dev.off()
   }
 }
